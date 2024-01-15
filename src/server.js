e7145cfa4e8323c5371dad1b0e03a825e0ce9375
MoulOut <mmoulinlima@outlook.com>
2024-01-15 18:21:30 -0300

    add Web Socket with socket.io

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import url from 'url';
 import path from 'path';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,17 @@ const actualPath = url.fileURLToPath(import.meta.url);
 const publicDirectory = path.join(actualPath, '../..', 'public');
 app.use(express.static(publicDirectory));
 
-app.listen(PORT, () => {
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+    console.log('A client has connected');
+    socket.on('disconnect',() =>{
+        console.log('A client has disconnected');
+    })
+})
